30865a1cd16e8222b186dfcb10afa2f5282dece4
吉留 健太 <yoshidomekenta@yoshitomekenta-no-iMac.local>
2016-04-14 03:34:25 +0900

    add promise + superagent

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var request = require('superagent');
 
 var toDoubleDigits = function(num) {
   num += ""; //Number型をString型に強制変換
@@ -8,7 +9,7 @@ var toDoubleDigits = function(num) {
 };
 
 var getNow = function(){
-  console.log('start');
+  console.log('現在時刻を表示します');
   var now = new Date();
   var year = now.getYear();
   if ( year < 2000){year += 1900};
@@ -21,8 +22,68 @@ var getNow = function(){
   console.log(date);
 }
 
-var nowDate = function (){
-  setInterval(getNow,5000);
+var getWeater = function() {
+  console.log('これから天気を取得します');
+  return new Promise(function(resolve, reject) {
+    return request.get('http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp')
+                  .accept('application/json')
+                  .end(function(err, res) {
+                    console.log(res.body);
+                    resolve()
+                    if (err) {
+                      reject(err);
+                    }
+                  });
+  });
+};
+
+var nowDate = function() {
+  setInterval(function() {
+    getNow();
+    getWeater().then(function() {
+      console.log('天気情報を取得しました');
+    },function(error) {
+      console.log('天気情報の取得に失敗しました')
+    });
+    // return new Promise(function(resolve, reject) {}
+  },5000);
 };
 
 nowDate();
+
+/*
+fetchBus = function() {
+  return new Promise(function(resolve, reject) {
+    return request.get('http://tutujibus.com/busLookup.php?busid=2').accept('application/json').end(function(err, res) {
+      var data;
+      if (err) {
+        reject(err);
+      }
+      data = res.text;
+      data = JSON.parse(data.slice(1, -1));
+      return resolve(data);
+    });
+  });
+};
+
+fetchSomething1(function() {fetchSomething2(function() {fetchSomething3(function() {fetchSomething4(doSomethingFinally); // 全部終わったら doSomethingFinally()
+    });
+  });
+})
+
+
+
+fetchAndUpload = function() {
+  return setInterval(function() {
+    return fetchBus().then(function(data) {
+      return writeFile(data);
+    }).then(function(filepath) {
+      return upload(filepath);
+    }).then(function() {
+      return console.log("done!");
+    })["catch"](function(err) {
+      return console.log("error:", err);
+    });
+  }, 60000);
+};;
+*/
